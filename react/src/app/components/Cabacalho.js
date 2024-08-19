1dcea58e7f84c2fb7dd106ca0d1460fd66b1dde7
andesoncerberos? <anderson_cerberos@hotmail.com>
2024-08-19 19:36:13 -0300

    comaçando meu primeiro react

---
+++ react/src/app/components/Cabacalho.js
@@ -0,0 +1,10 @@
+export default function Cabecalho(){
+    return(
+        <>
+        <div>
+            <h1>cabeçalho</h1>
+            <p>isto e um cabeçalho da miha pagina</p>
+        </div>
+        </>
+    )
+}
---
+++ react/src/app/fundamentos/page.js
@@ -0,0 +1,14 @@
+export default function fundamentos(){
+
+    
+    return (
+        <>
+            <Cabecalho /> 
+            <h1>fundamentos</h1>
+            <p>sucesso</p>
+            <Cabecalho />
+        </>
+       
+    )
+}
+
