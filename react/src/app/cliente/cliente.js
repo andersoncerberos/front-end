d20471bb4ba8f4cbcc10c4243c97678eb9d7939d
andersoncerberos <anderson_cerberos@hotmail.com>
2024-08-21 18:39:47 -0300

    feita a pagina clientes

--- react/src/app/cliente/cliente.js
+++ react/src/app/cliente/cliente.js
@@ -1,13 +1,42 @@
 import Link from "next/link";
-import Cabecalho from "../components/Cabacalho";
+import Cabecalho from "../components/Cabecalho";
+import Table from 'react-bootstrap/Table';
 
-export default function Cliente(){
-
-    return(
-        <Cabecalho>
-            <Link href ="/">pagina inicial</Link>
-            <Link href ="/fundamentos">pagina fundamentos</Link>
-        </Cabecalho>
+export default function Clientes() {
+    return (
+        <>
+            <Cabecalho titulo="Clientes" sub="Página de clientes usando o cabeçalho"></Cabecalho>
+            <Link href="/">Página Inicial</Link><br />
+            <Link href="../fundamentos">Fundamentos</Link><br />
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </>
     )
-
 }
